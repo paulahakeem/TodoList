e22b1a7acb9418d028733635c7cca227ac38e44d
marwaahmed11 <marwaahmed200126@gmail.com>
2021-12-31 14:08:55 +0200

    first commit

---
+++ final-main/server/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+    try {
+        const connectionParams = {
+           // useNewUrlParser: true,
+           // useCreateIndex: true,
+            //useUnifiedTopology: true,
+        };
+        await mongoose.connect(
+          // "mongodb://localhost/final-main",
+           "mongodb://"+process.env.ENV_PORT+":27017/?directConnection=true&serverSelectionTimeoutMS=2000",
+            connectionParams
+        );
+        console.log("Connected to database.");
+    } catch (error) {
+        console.log("Could not connect to database.", error);
+    }
+};
---
+++ final-main/server/models/task.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+    task: {
+        type: String,
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+
+const pro= mongoose.model("task", taskSchema);
+module.exports = pro;
